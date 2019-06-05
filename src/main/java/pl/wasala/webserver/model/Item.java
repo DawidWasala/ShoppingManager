dc1d552562f4f51224600444c04dac98c75ae234
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2019-06-05 19:29:52 +0200

    Code refactor

--- src/main/java/pl/wasala/webserver/model/Item.java
+++ src/main/java/pl/wasala/webserver/model/Item.java
@@ -10,14 +10,38 @@ public class Item {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
     private String name;
-    private int price;
+    private Integer price;
 
-    protected Item(){}
+    private Item(){}
 
-    public Item(String name, int price) {
+    public Item(String name, Integer price) {
         this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
         this.price = price;
     }
 
--- src/main/java/pl/wasala/webserver/repository/ItemRepository.java
+++ src/main/java/pl/wasala/webserver/repository/ItemRepository.java
@@ -1,9 +1,9 @@
 package pl.wasala.webserver.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import pl.wasala.webserver.model.Item;
 
-@Repository
-public interface ItemRepository extends JpaRepository<Item,Long> {
+@RepositoryRestResource
+public interface ItemRepository extends CrudRepository<Item,Long> {
 }
